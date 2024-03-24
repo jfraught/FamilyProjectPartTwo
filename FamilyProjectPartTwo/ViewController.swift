c90bd5ef64731ff7c0b1ee0bbcf70219505eda11
jfraught <fraughtdawg@gmail.com>
2024-03-24 14:34:14 -0600

    Initial Commit

---
+++ FamilyProjectPartTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyProjectPartTwo
+//
+//  Created by Jordan Fraughton on 3/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
